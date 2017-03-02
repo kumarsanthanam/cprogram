ea48462564321f4c169342ed42a15124da8eeeb9
kumar <kumar.mcasi@gmail.com>
2017-03-02 23:52:45 +0530

    changes in test.c

--- test.c
+++ test.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 
 int main(){
+
 	printf("linux world\n");
 	printf("checking success\n");
 	printf("test succeed\n");
 	printf("ssh\n");
+	printf("sdn world\n");
+
 	return 0;
 }
